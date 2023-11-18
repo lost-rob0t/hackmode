df3df645410dfe1ea97e464f5bc0c631a56b4072
nsaspy <nsaspy@fedora.email>
2023-11-17 21:07:09 -0500

    add exports

--- source/hackmode-core/package.lisp
+++ source/hackmode-core/package.lisp
@@ -14,7 +14,46 @@
    :EXPLOIT-FUNC
    :EXPLOIT-HELP
    :EXPLOIT-PLATFORM
-   :EXPLOIT-OPTION-TYPE))
+   :EXPLOIT-OPTION-TYPE
+   :hackmode-init-file
+   :hackmode-operations-database
+   :RHOST
+   :payloads
+   :config-dir
+   :wordlist-dir
+   :wordlist-alist
+   :wordlist
+   :target-platform
+   :socks5-proxy-list
+   :http-proxy-list
+   :exploits-dir
+   :history-path
+   :prompt
+   :*startup-hook*
+   :init-database
+   :put-doc
+   :put-docs
+   :*db*
+   :*operations-database*
+   :meta
+   :output
+   :host
+   :port
+   :finding
+   :url
+   :*api-common-patterns*
+   :find-api
+   :find-apis
+   :*exploits*
+   :*current-exploit*
+   :exploit-option
+   :exploit
+   :operation
+   :operation-dir
+   :domain
+   :domain-name
+   :domain-type
+   :doman-zone))
 
 
 (in-package :hackmode)
