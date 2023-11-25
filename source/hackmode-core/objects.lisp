465ee60e0f5372c5e5be15a90fd4c36f90b04f0b
N545PY <nsaspy@fedora.email>
2023-11-25 18:48:38 -0500

    add cert object

--- source/hackmode-core/objects.lisp
+++ source/hackmode-core/objects.lisp
@@ -42,8 +42,18 @@
 
 
 (defclass url (meta)
-  ((scheme :initarg :scheme :type string :accessor url-scheme :allocation :class)
-   (host :initarg :host :type string :accessor url-host :allocation :class)
-   (port :initarg :port :type integer :accessor url-port :allocation :class)
-   (path :initarg :path :type string :accessor url-path :allocation :class)
-   (query :initarg :query :type string :accessor url-query :allocation :class)))
+  ((scheme :initarg :scheme :type string :reader url-scheme :allocation :class)
+   (host :initarg :host :type string :reader url-host :allocation :class)
+   (port :initarg :port :type integer :reader url-port :allocation :class)
+   (path :initarg :path :type string :reader url-path :allocation :class)
+   (query :initarg :query :type string :reader url-query :allocation :class)))
+
+
+(defclass cert (meta)
+  ((not-before :initarg :not-before :type integer :accessor cert-not-before :initform (unix-now) :allocation :class)
+   (not-after :initarg :not-after :type integer :accessor cert-not-after :initform (unix-now) :allocation :class)
+   (common-name :initarg :common-name :type string :accessor cert-common-name :initform "" :allocation :class)
+   (org-unit-name :initarg :org-unit-name :type string :accessor cert-org-unit-name :initform "" :allocation :class)
+   (locality :initarg :locality :type string :accessor cert-locality :initform "" :allocation :class)
+   (country-name :initarg :country :type string :accessor cert-country :initform "" :allocation :class)
+   (province :initarg :province :type string :accessor cert-province :initform "" :allocation :class)))
