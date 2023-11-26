e0f87d64aeafacdd6505ac1412dd012d18a0a2e6
N545PY <nsaspy@fedora.email>
2023-11-25 23:52:06 -0500

    add build stuff

---
+++ Makefile
@@ -0,0 +1,20 @@
+##
+# Hackmode
+#
+# @file
+# @version 0.1
+
+LISP ?= sbcl
+
+all: test
+
+run:
+	$(LISP) --load run.lisp
+
+build:
+	$(LISP)	--non-interactive \
+		--load hackmode-user.asd \
+		--eval '(ql:quickload :hackmode-user)' \
+		--load 'hackmode.lisp' \
+		--eval "(sb-ext:save-lisp-and-die \"hm\" :toplevel 'hackmode-user::main :executable t :compression t)"
+# end
--- hackmode.lisp
+++ hackmode.lisp
@@ -1,3 +1,4 @@
+(declaim (optimize (speed 3) (safety 0)))
 (defpackage   :hackmode-user
   (:export :main)
   (:use :cl #:hackmode)
@@ -5,11 +6,12 @@
 ;; I dont really use hackmode-user
 (in-package :hackmode-user)
 
+
 (defun main ()
   (in-package :cl-user)
   (use-package :hackmode)
   (use-package :hackmode-tools)
-  (setq *interactive* t)
+  (setq hackmode::*interactive* t)
   (load hackmode-init-file)
   (nhooks:run-hook *startup-hook*)
   (lish:lish))
