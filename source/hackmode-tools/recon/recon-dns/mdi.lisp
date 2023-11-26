7ddaba7a81b36138c78dbf714ccd1576360fbee4
N545PY <nsaspy@fedora.email>
2023-11-26 15:25:37 -0500

    add hooks to check-mdi
    Also no longer exports "tenets" in a list, makes no sense
    check-mdi* will tag azure domains

--- source/hackmode-tools/recon/recon-dns/mdi.lisp
+++ source/hackmode-tools/recon/recon-dns/mdi.lisp
@@ -1,5 +1,15 @@
 (in-package :recon.dns)
 
+(defvar *check-mdi-setup-hook* (make-instance 'nhooks:hook-void
+                                              :handlers nil)
+  "Hook That is called before check-mdi is ran.")
+
+(defvar *check-mdi-finish-hook* (make-instance 'nhooks:hook-any
+                                               :handlers nil)
+  "Hook That is called after check-mdi is finished. Argument must accept list of DOMAIN objects.")
+
+
+
 (defun make-mdi-body (domain)
   (format nil "<?xml version=\"1.0\" encoding=\"utf-8\"?>
     <soap:Envelope xmlns:exm=\"http://schemas.microsoft.com/exchange/services/2006/messages\"
@@ -26,6 +36,8 @@
     </soap:Envelope>" domain))
 
 
+
+
 (defun make-mdi-request (domain)
   (let* ((body (make-mdi-body domain))
          (headers '(("Content-type" . "text/xml; charset=utf-8")
@@ -45,22 +57,18 @@
 
                                                                                                          (xmls:xmlrep-find-child-tag "Body" (xmls:parse resp)))))))
         if domain collect (nth 2 domain)))
+(defun check-mdi (domain)
+  (remove-duplicates (parse-mdi-resp  (make-mdi-request domain)) :test #'string=))
 
-(defun check-mdi (domain &key (tenets t) (domains t))
-  (let* ((resp (make-mdi-request domain))
-         (domains (parse-mdi-resp resp))
-         (tenets (remove-duplicates (when tenets
-                                      (loop for domain in domains
-                                            when (search "onmicrosoft.com" domain)
-                                              collect (first (uiop:split-string domain :separator ".")))) :test #'string=))
-         (domains
-           (remove-duplicates  (when domains
-                                 (loop for domain in domains
-                                       if (not (search "onmicrosoft.com" domain))
-                                         collect domain)) :test #'string=)))
-    (when *interactive*
-      (format t "====Tenets=====~% ~{Tenet: ~a~%~}" tenets)
-      (format t "====Domains====~% ~{Domain: ~a~%~}" domains))
 
-    (list :domains domains :tenets tenets)))
 
+(defun check-mdi* (domain)
+  (nhooks:run-hook *check-mdi-setup-hook*)
+  (let ((records
+          (loop for domain in (check-mdi domain)
+                for record = (make-instance 'domain :id (format nil "~a" (sxhash domain)) :dtype "domain" :record domain :tags (list "domain" "cert.sh"))
+                do (if (str:containsp "onmicrosoft.com" domain) (push "azure-tenet" (doc-tags record)))
+                do (nhooks:run-hook *domain-hook* record)
+                collect record)))
+    (nhooks:run-hook *check-mdi-finish-hook* records)
+    records))
