2d04b1993b9a4349ea77710e0139fbe5261b39b7
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-16 01:32:14 -0400

    route crtsh discoveries through canonical assets

--- source/hackmode-tools/recon/recon-dns/certsh.lisp
+++ source/hackmode-tools/recon/recon-dns/certsh.lisp
@@ -1,33 +1,48 @@
 (in-package :recon.dns)
 
-
 (defun cert.sh (domain)
-  (remove-duplicates (flatten (loop for result in  (jsown:parse (dex:get (format nil "https://crt.sh/?q=~a&output=json" domain) :headers '(("User-Agent" . "Hackmode") ("Accept" . "Application/json"))))
-                                    for domain = (jsown:val result "name_value")
-                                    if (not (str:containsp "@" domain))
-                                      collect (str:words (cl-ppcre:regex-replace-all "\\*\\."  domain "")))) :test #'string=))
+  "Return unique domain names observed by crt.sh for DOMAIN."
+  (remove-duplicates
+   (flatten
+    (loop for result in
+            (jsown:parse
+             (dex:get
+              (format nil "https://crt.sh/?q=~a&output=json" domain)
+              :headers '(("User-Agent" . "Hackmode")
+                         ("Accept" . "Application/json"))))
+          for name = (jsown:val result "name_value")
+          unless (str:containsp "@" name)
+            collect
+            (str:words
+             (cl-ppcre:regex-replace-all "\\*\\." name ""))))
+   :test #'string=))
 
+(defvar *cert.sh-setup-hook*
+  (make-instance 'nhooks:hook-void :handlers nil)
+  "Hook called before crt.sh lookup runs.")
 
-(defvar *cert.sh-setup-hook* (make-instance 'nhooks:hook-void
-                                            :handlers nil)
-  "Hook That is called before cert.sh is ran.")
-
-(defvar *cert.sh-finish-hook* (make-instance 'nhooks:hook-any
-                                             :handlers nil)
-  "Hook That is called after cert.sh is finished. Argument must accept list of DOMAIN objects.")
-
-
+(defvar *cert.sh-finish-hook*
+  (make-instance 'nhooks:hook-any :handlers nil)
+  "Hook called with the list of discovered DOMAIN objects.")
 
 (defun cert.sh* (domain)
+  "Query crt.sh and route domains through Hackmode's canonical asset lifecycle."
   (nhooks:run-hook *cert.sh-setup-hook*)
-  (let ((resp (loop for domain in (cert.sh domain)
-                    for result = (make-instance 'domain :id (format nil "~a" (sxhash domain)) :record domain  :tool "cert.sh" :tags '("crt.sh" "domain"))
-                    do (nhooks:run-hook *domain-hook* result)
-                    collect result)))
-
-    (nhooks:run-hook *cert.sh-finish-hook* resp)
-    resp))
+  (let ((response
+          (loop for name in (cert.sh domain)
+                unless (string= name "")
+                  collect
+                  (multiple-value-bind (stored created-p persisted-p)
+                      (record-recon-asset
+                       (make-instance 'domain
+                                      :record name
+                                      :tool "crt.sh"
+                                      :tags '("crt.sh" "domain")))
+                    (declare (ignore created-p persisted-p))
+                    stored))))
+    (nhooks:run-hook *cert.sh-finish-hook* response)
+    response))
 
 (lish:defcommand cert.sh ((domain string))
-  (loop for domain in  (cert.sh* domain)
-        do (format t "~a~%" (domain-name domain))))
+  (loop for result in (cert.sh* domain)
+        do (format t "~a~%" (domain-name result))))
