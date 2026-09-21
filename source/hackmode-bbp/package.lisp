a39abcaa465cca68c31b455181e0954f220870dd
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-20 23:13:13 -0400

    feat(bbp): add package.lisp

---
+++ source/hackmode-bbp/package.lisp
@@ -0,0 +1,25 @@
+(uiop:define-package :hackmode-bbp
+  (:use :cl)
+  (:export
+   :bbp-target
+   :make-bbp-target
+   :bbp-target-id
+   :bbp-target-actor
+   :bbp-target-value
+   :bbp-target-dataset
+   :bbp-target-sources
+   :bbp-target-options
+   :bbp-scan-result
+   :bbp-scan-result-target
+   :bbp-scan-result-state
+   :bbp-scan-result-job-id
+   :bbp-scan-result-assets
+   :bbp-scan-result-documents
+   :bbp-scan-result-error
+   :bbp-target-plan
+   :start-bbp-supervisor
+   :stop-bbp-supervisor
+   :dispatch-bbp-target
+   :*bbp-supervisor*))
+
+(in-package :hackmode-bbp)
